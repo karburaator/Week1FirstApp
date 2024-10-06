20693df1ff086aad9473b8ae86d08bf06ae0eec8
karburaator <example@mail.com>
2024-10-06 12:39:46 +0300

    add my first app

---
+++ Week1FirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string myName = "Harry";
+string myLastName = "Potter";
+
+//Console.WriteLine("Hello" + " " + myName);
+
+Console.WriteLine($"Hello, {myName} {myLastName}!");
